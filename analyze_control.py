c6bb9debac519dda055f482c64eb65478cefdd44
Hisashi Miura <hisashi.office@gmail.com>
2019-04-03 20:42:12 +0900

    add region non occupant room temperature

--- analyze_control.py
+++ analyze_control.py
@@ -303,6 +303,8 @@ def get_each_supply_air_volume_for_cooling(cn: int) -> np.ndarray:
 # endregion
 
 
+# region non occupant room temperature
+
 def get_non_occupant_room_temperature_for_heating(cn: int) -> np.ndarray:
     region, floor_area, envelope_spec, system_spec = get_spec(cn)
     return cs.get_non_occupant_room_temperature_for_heating(region, floor_area, envelope_spec, system_spec)
@@ -311,6 +313,9 @@ def get_non_occupant_room_temperature_for_heating(cn: int) -> np.ndarray:
 def get_non_occupant_room_temperature_for_cooling(cn: int) -> np.ndarray:
     region, floor_area, envelope_spec, system_spec = get_spec(cn)
     return cs.get_non_occupant_room_temperature_for_cooling(region, floor_area, envelope_spec, system_spec)
+
+# endregion
+
 
 
 def get_heat_source_heating_output(cn: int) -> np.ndarray:
