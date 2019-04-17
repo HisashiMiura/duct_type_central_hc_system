d2c348e18c7807fdf8c001f69e5d5c78317f7bfa
Hisashi Miura <hisashi.office@gmail.com>
2019-04-17 18:25:31 +0900

    add main

---
+++ test_main.py
@@ -0,0 +1,26 @@
+import unittest
+import numpy as np
+import pandas as pd
+
+import analyze_control as ac
+
+class TestMain(unittest.TestCase):
+
+    def test_total_result(self):
+
+        CASE = 6
+
+        result = ac.get_main_value(CASE)
+
+        t = result['time_value']
+
+        df_t = pd.DataFrame(t)
+
+        self.assertAlmostEqual(5.7031, df_t.heating_load_room1[0])
+        self.assertAlmostEqual(22.3080687992392, df_t.output_of_heat_source_heating[0])
+        self.assertAlmostEqual(2.74914120989784, df_t.output_of_heat_source_sensible_cooling[4798])
+        self.assertAlmostEqual(1.87939, df_t.output_of_heat_source_latent_cooling[4798])
+
+
+if __name__ == '__main__':
+    unittest.main()
